a9116e83eeef01c22c0feb76ab70835cc74dc64e
ann-shen <yazu0419@gmail.com>
2022-05-29 22:33:31 +0800

    fix register status by  get username

--- camping-go/src/App.js
+++ camping-go/src/App.js
@@ -1,6 +1,6 @@
 import { UserContext } from "./utils/userContext";
-import { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes,Navigate } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
@@ -21,9 +21,8 @@ function App() {
   const [userName, setUserName] = useState("");
   const [groupId, setGroupId] = useState("");
   const [userId, setUserId] = useState("");
+  const [loginStatus, setLoginStatus] = useState(false);
   const auth = getAuth();
-  console.log(userName);
-  
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -35,54 +34,58 @@ function App() {
         console.log("logout");
       }
     });
-  }, []);
+    setLoginStatus(true);
+  }, [groupId]);
 
+  useEffect(() => {
+    console.log(userName);
+  }, [loginStatus]);
+
+  if (userName !== null) {
+    console.log(userName);
+  }
+  
   const value = {
     userId,
     userName,
     personName: "",
   };
 
+
   return (
     <div className='App'>
       <UserContext.Provider value={value}>
         <BrowserRouter>
           <ScrollToTop>
-          <Routes>
-            <Route
-              path='/create_group'
-              element={
-                <CreateGroup />
-              }></Route>
-            <Route
-              path='/'
-              element={<CampingGroup setGroupId={setGroupId} />}></Route>
-            <Route
-              path='joinGroup/:id'
-              element={<JoinGroupPage userName={userName} />}></Route>
-            <Route
-              path='profile/:id'
-              element={<Profile />}></Route>
-            <Route
-              path='login'
-              element={
-                <Login
-                  setUserId={setUserId}
-                  setUserName={setUserName}
-                  userName={userName}
-                />
-              }></Route>
-            <Route
-              path='/:city'
-              element={<CityCamping groupId={groupId} userId={userId} />}
-            />
-            <Route path='find_group' element={<FindGroup />} />
-            <Route
-              path='second_hand'
-              element={<SecondHand userName={userName} userId={userId} />}
-            />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
+            <Routes>
+              <Route path='/create_group' element={<CreateGroup />}></Route>
+              <Route
+                path='/'
+                element={<CampingGroup setGroupId={setGroupId} />}></Route>
+              <Route
+                path='joinGroup/:id'
+                element={<JoinGroupPage userName={userName} />}></Route>
+              <Route path='profile/:id' element={<Profile />}></Route>
+              <Route
+                path='login'
+                element={
+                  <Login
+                    setUserId={setUserId}
+                    setUserName={setUserName}
+                    userName={userName}
+                  />
+                }></Route>
+              <Route
+                path='/:city'
+                element={<CityCamping groupId={groupId} userId={userId} />}
+              />
+              <Route path='find_group' element={<FindGroup />} />
+              <Route
+                path='second_hand'
+                element={<SecondHand userName={userName} userId={userId} />}
+              />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
           </ScrollToTop>
         </BrowserRouter>
       </UserContext.Provider>
